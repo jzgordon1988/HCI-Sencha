eb05a68407bcb9a7baf9a4f58abafcc50c47bd9c
Justin Gordon <jgordon@consultwithhoward.com>
2014-01-20 17:42:16 -0500

    first test commit

---
+++ app/view/Contact.js
@@ -0,0 +1,49 @@
+Ext.define('GS.view.Contact', {
+	extend: 'Ext.form.Panel',
+	xtype: 'contactform',
+	
+	requires: [
+		'Ext.form.FieldSet',
+		'Ext.field.Email'
+	],
+	
+	config: {
+		title: 'Contact',
+		iconCls: 'user',
+		url: 'contact.php',
+		
+		items: [
+			{
+				xtype: 'fieldset',
+				title: 'Contact Us',
+				instructions: '(Email Address is optional)',
+			
+				items: [
+					{
+						xtype: 'textfield',
+						name: 'name',
+						label: 'Name'
+					},
+					{
+						xtype: 'emailfield',
+						name: 'email',
+						label: 'Email'
+					},
+					{
+						xtype: 'textareafield',
+						name: 'message',
+						label: 'Message'
+					}
+				]
+			},
+			{
+				xtype: 'button',
+				text: 'Send',
+				ui: 'confirm',
+				handle: function() {
+					this.up('contactform').submit();
+				}
+			}
+		]
+	}
+});
